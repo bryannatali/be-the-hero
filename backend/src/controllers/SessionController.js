c3b7bf5e0635e1e19102934746c5cb03905b2b65
Bryan <bryannatali01@gmail.com>
2020-03-25 11:17:05 -0300

    Frontend da aplicação

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -12,6 +12,6 @@ module.exports = {
         if(!ong)
             return res.status(400).json({ error: 'No ONG found with this ID.'});
 
-        return res.json({ ong });
+        return res.json(ong);
     }
 }
